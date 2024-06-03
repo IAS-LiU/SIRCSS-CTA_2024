3c80402af85bf14255992405b3536956d926dab1
ninpnin <vainoyrjanainen@icloud.com>
2024-06-03 15:43:48 +0200

    feat: start model solution

--- src/train_dynamic.py
+++ src/train_dynamic.py
@@ -3,6 +3,7 @@ import probabilistic_word_embeddings as pwe
 from probabilistic_word_embeddings.preprocessing import preprocess_standard, preprocess_partitioned
 from probabilistic_word_embeddings.embeddings import LaplacianEmbedding
 from probabilistic_word_embeddings.estimation import map_estimate
+from probabilistic_word_embeddings.evaluation import evaluate_on_holdout_set
 import pandas as pd
 import numpy as np
 
@@ -11,33 +12,35 @@ VAL_SPLIT = 0.2
 YEAR0 = 2005
 YEAR1 = 2012
 
-df = pd.read_csv("articles_en.csv").head(10000)
+df = pd.read_csv("articles_en.csv").head(100000)
 
+# Extract year from the 'Date' column
 df["year"] = df["Date"].str.split("/").str[0]
 print(df)
 
+# Drop null rows
 df = df.dropna()
 
 ## Create train-val split
 df["val"] = np.random.rand(len(df)) <= VAL_SPLIT
-df_train = df[df["val"] == False]
-df_val = df[df["val"] == True]
 
-texts_train = [t.split() for t in df_train["Text"]]
-labels_train = [y for y in df_train["year"]]
+texts = [t.split() for t in df["Text"]]
+labels = [y for y in df["year"]]
 print(df)
 
 ## 
-texts_train, vocab = preprocess_partitioned(texts_train, labels=labels_train, downsample=False)
+texts, vocab = preprocess_partitioned(texts, labels=labels, downsample=False)
 
 #print(vocab)
-traindata = []
-for t in texts_train:
-    traindata += t
+traindata, valdata = [], []
+for t, is_validation in zip(texts, df["val"]):
+    if is_validation:
+        valdata += t
+    else:
+        traindata += t
 
 print(traindata[:10])
 print(traindata[-10:])
-
 
 
 ## Generate prior graph
@@ -45,17 +48,27 @@ g = nx.Graph()
 years = range(YEAR0, YEAR1)
 
 for year0, year1 in zip(years[:-1], years[1:]):
-    for wd in set([wd.split("_")[0] for wd in vocabulary]):
+    for wd in set([wd.split("_")[0] for wd in vocab]):
         wd0 = f"{wd}_{year0}"
         wd1 = f"{wd}_{year1}"
-        if wd0 in vocabulary and wd1 in vocabulary:
+        if wd0 in vocab and wd1 in vocab:
             g.add_edge(wd0, wd1)
 
 print(list(g.edges)[:10])
 
+best_val_likelihood = -10000
+e_best = None
+
 ## Grid search
-for l1 in [50, 100, 250, 500, 1000]:
+for l1 in [50, 100, 250]:
+    print("Prior strength: ", l1)
     e = LaplacianEmbedding(vocab, graph=g, dimensionality=100, lambda1=l1)
-    e = map_estimate(e, traindata, epochs=5)
-    hold
-    print()
+    e = map_estimate(e, traindata, epochs=5, batch_size=5000)
+    val_likelihood = evaluate_on_holdout_set(e, valdata)
+
+    print(val_likelihood)
+    if val_likelihood > best_val_likelihood:
+        best_val_likelihood = val_likelihood
+        e_best = e
+
+print(e_best)
