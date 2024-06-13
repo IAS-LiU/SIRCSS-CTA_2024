148398e55f34dd007c59d679eb94736dd0baa68b
Vinö <ninpnin@users.noreply.github.com>
2024-06-13 21:30:35 +0200

    docs: add comments

--- src/step2_train.py
+++ src/step2_train.py
@@ -7,20 +7,30 @@ from probabilistic_word_embeddings.evaluation import evaluate_on_holdout_set
 import json, itertools
 import numpy as np
 
+# Specify hyperparameters
 DIM = 100
 LAMBDA1 = 200.0
 
+# Load data
 with open('data.json') as f:
     data = json.load(f)
-
 texts, vocabulary = data["texts"], data["vocabulary"]
+
+# Load prior graph
 g = nx.read_adjlist("prior.graph")
 
-#print(len())
+# Define embedding. Use Laplacian embedding since we have an informative prior
 e = LaplacianEmbedding(vocabulary, graph=g, dimensionality=DIM, lambda1=LAMBDA1)
+
+# Concatenate the list of lists into one list
+# i.e. [["the_2006", ..., "done_2006"], ... , ["yesterday_2012", ..., "followed_2012"]]
+# into ["the_2006", ... "followed_2012"]
 train_data = list(itertools.chain(*texts))
+
+# Train / estimate embeddings using MAP estimation
+# use model="sgns" if you want the SGNS variant
 e = map_estimate(e, train_data, model="cbow", ws=5, epochs=2, batch_size=20000)
 
+# Save resulting embedding
 e.save("embedding-cbow.pkl")
-#e.save("embedding.json")
 
